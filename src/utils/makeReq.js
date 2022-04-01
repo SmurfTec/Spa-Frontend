81757e21a18cba800a209a2abb3bc99d4a724b78
muhammadzain8 <muhammadzain8@gmail.com>
2022-04-01 14:18:05 +0500

    live api fixed

--- src/utils/makeReq.js
+++ src/utils/makeReq.js
@@ -2,17 +2,18 @@ import { LOCALSTORAGE_TOKEN_KEY } from './constants';
 import { toast } from 'react-toastify';
 
 // * Development URLs
-const API_BASE_URL = `http://localhost:5000/api`;
-const API_BASE_ORIGIN = `http://localhost:5000`;
+// const API_BASE_URL = `http://localhost:5000/api`;
+// const API_BASE_ORIGIN = `http://localhost:5000`;
 
 // * Production URLs
-// const API_BASE_URL = `https://spa-api-1.herokuapp.com/api`;
-// const API_BASE_ORIGIN = `https://spa-api-1.herokuapp.com/`;
+const API_BASE_URL = `https://spa-api-1.herokuapp.com/api`;
+const API_BASE_ORIGIN = `https://spa-api-1.herokuapp.com/`;
 
 const handleCatch = (err) => {
   let errMsg = 'Something Went Wrong';
   if (err.message) errMsg = err.message;
-  if (err.response?.data?.message) errMsg = err.response?.data?.message;
+  if (err.response?.data?.message)
+    errMsg = err.response?.data?.message;
   toast.error(errMsg);
 };
 
@@ -40,14 +41,16 @@ const makeReq = (
   if (body) {
     config.body = JSON.stringify(body);
   }
-  return fetch(`${API_BASE_URL}${endpoint}`, config).then(async (res) => {
-    const data = await res.json();
-    if (res.ok) {
-      return data;
-    } else {
-      return Promise.reject(data);
+  return fetch(`${API_BASE_URL}${endpoint}`, config).then(
+    async (res) => {
+      const data = await res.json();
+      if (res.ok) {
+        return data;
+      } else {
+        return Promise.reject(data);
+      }
     }
-  });
+  );
 };
 
 export { API_BASE_URL, API_BASE_ORIGIN, makeReq, handleCatch };
